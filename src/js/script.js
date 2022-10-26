8913b509e815a2644c16ca2936efdff4fcbfde16
Lilp Su <lilp@blockstudio.tw>
2022-10-27 00:17:37 +0800

    feat: add announce

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,18 @@ document.querySelectorAll(".float__group a").forEach((el) => {
 document.querySelector(".to-top").addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
+
+document.querySelectorAll(".announce__wrapper h2").forEach((el) => {
+  console.log(el.dataset.date);
+
+  const targetDate = new Date(el.dataset.date);
+  const today = new Date();
+
+  if (targetDate.toDateString() === today.toDateString()) {
+    el.classList.add("today");
+  }
+
+  if (targetDate < today) {
+    el.classList.add("expired");
+  }
+});
